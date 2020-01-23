6d7466eb424a1c72bff0c7ca4f72332a0436ac12
NGUYEN CONG TRUONG <nctruong861990@gmail.com>
2020-01-23 17:05:20 +0700

    update spec for random bookings

--- spec/assign_room_spec.rb
+++ spec/assign_room_spec.rb
@@ -2,22 +2,43 @@ require_relative './spec_helper'
 
 describe 'AssignRoom' do
   let!(:bookings) { JSON.parse(File.read("data/input.txt"), symbolize_names: true) }
+  let!(:random_bookings) { JSON.parse(File.read("data/random_bookings.txt"), symbolize_names: true) }
 
   describe '#call' do
-    before do
-      @results = AssignRoom.call(bookings, 3)
+    context 'when bookings in ascending order of checkin' do
+      before do
+        @results = AssignRoom.call(bookings, 3)
+      end
+
+      it 'returns first booking collection equals [1,3,6,8,9]' do
+        expect(@results[0]).to eq([1,3,6,8,9])
+      end
+
+      it 'returns second booking collection equals [2,5,7]' do
+        expect(@results[1]).to eq([2,5,7])
+      end
+
+      it 'returns last booking collection equals [4]' do
+        expect(@results[2]).to eq([4])
+      end
     end
 
-    it 'returns first booking equals [1,3,6,8,9]' do
-      expect(@results[0]).to eq([1,3,6,8,9])
-    end
+    context 'when bookings in random order of checkin' do
+      before do
+        @results = AssignRoom.call(random_bookings, 3)
+      end
 
-    it 'returns second booking equals [2,5,7]' do
-      expect(@results[1]).to eq([2,5,7])
-    end
+      it 'returns first booking collection equals [1,3,6,8,9]' do
+        expect(@results[0]).to eq([1,3,6,8,9])
+      end
 
-    it 'returns last booking equals [4]' do
-      expect(@results[2]).to eq([4])
+      it 'returns second booking collection equals [2,5,7]' do
+        expect(@results[1]).to eq([2,5,7])
+      end
+
+      it 'returns last booking collection equals [4]' do
+        expect(@results[2]).to eq([4])
+      end
     end
   end
 end
